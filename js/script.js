e4af75aa25d8bcd6362c635d26a6d0951fed39a5
v0lv3r1n3 <patryk.kosmalski007@gmail.com>
2023-05-28 20:57:55 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function enforceNumberValidation(ele) {
+    if ($(ele).data('decimal') != null) {
+        // found valid rule for decimal
+        var decimal = parseInt($(ele).data('decimal')) || 0;
+        var val = $(ele).val();
+        if (decimal > 0) {
+            var splitVal = val.split('.');
+            if (splitVal.length == 2 && splitVal[1].length > decimal) {
+                // user entered invalid input
+                $(ele).val(splitVal[0] + '.' + splitVal[1].substr(0, decimal));
+            }
+        } else if (decimal == 0) {
+            // do not allow decimal place
+            var splitVal = val.split('.');
+            if (splitVal.length > 1) {
+                // user entered invalid input
+                $(ele).val(splitVal[0]); // always trim everything after '.'
+            }
+        }
+    }
+}
